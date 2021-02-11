278422ac5b2ed9cc6e12f5918c3cbc8770b1a81e
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-02-10 16:09:24 -0800

    Updates

---
+++ precache-manifest.f05cce2b93a618926bbc6f06950e6122.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5198b5fc4e6a843b6f488362c20a6d54",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "64c23f2de762f783c992",
+    "url": "/weather_app/static/css/2.3bc231fd.chunk.css"
+  },
+  {
+    "revision": "2879d7cd44594a7ccf74",
+    "url": "/weather_app/static/css/main.1d9a4c90.chunk.css"
+  },
+  {
+    "revision": "64c23f2de762f783c992",
+    "url": "/weather_app/static/js/2.46b07ff0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/weather_app/static/js/2.46b07ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2879d7cd44594a7ccf74",
+    "url": "/weather_app/static/js/main.2d894e57.chunk.js"
+  },
+  {
+    "revision": "4cc83ff11d5c1d5ff950",
+    "url": "/weather_app/static/js/runtime-main.c56cf221.js"
+  },
+  {
+    "revision": "75f9c176533fd857a6e516a8aa2df512",
+    "url": "/weather_app/static/media/sky photo.75f9c176.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/weather_app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/weather_app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/weather_app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/weather_app/static/media/slick.f97e3bbf.svg"
+  }
+]);
